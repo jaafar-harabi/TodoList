96e37f3b8c417f32bb4c09238541553f342f3795
jaafar-harabi <jaafar.harabi@enig.u-gabes.tn>
2023-05-21 15:15:55 +0100

    Update connect.js

--- config/connect.js
+++ config/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose') ;
 
-mongoose.connect('mongodb://127.0.0.1/todoList')
+mongoose.connect(process.env.DB)
     .then(
         ()=>{console.log('connect to db')}
     ).catch(
